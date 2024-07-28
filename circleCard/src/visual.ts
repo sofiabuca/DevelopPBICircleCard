84ff818cf579b7da64aed6a24a85330b718de0ac
Sofia Buitrago <laurabuca98@gmail.com>
2024-07-27 22:13:06 -0600

    - Visuals

--- circleCard/src/visual.ts
+++ circleCard/src/visual.ts
@@ -24,54 +24,120 @@
 *  THE SOFTWARE.
 */
 "use strict";
-
-import powerbi from "powerbi-visuals-api";
-import { FormattingSettingsService } from "powerbi-visuals-utils-formattingmodel";
 import "./../style/visual.less";
-
-import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
-import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import powerbi from "powerbi-visuals-api";
+import VisualContructorOptions=powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions =  powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
+import DataView = powerbi.DataView;
+import IVisualHost = powerbi.extensibility.IVisualHost;
+import * as d3 from "d3";
+type Selection <T extends d3.BaseType> = d3.Selection <T, any,any,any>;
 
-import { VisualFormattingSettingsModel } from "./settings";
 
-export class Visual implements IVisual {
-    private target: HTMLElement;
-    private updateCount: number;
-    private textNode: Text;
-    private formattingSettings: VisualFormattingSettingsModel;
-    private formattingSettingsService: FormattingSettingsService;
+//Implements IVisual interface
+export class Visual implements IVisual{
+    //Variables
+    private host: IVisualHost;
+    private svg: Selection<SVGElement>;
+    private container: Selection<SVGElement>;
+    private circle: Selection<SVGElement>;
+    private textValue: Selection<SVGElement>;
+    private textLabel: Selection<SVGElement>;
 
-    constructor(options: VisualConstructorOptions) {
-        console.log('Visual constructor', options);
-        this.formattingSettingsService = new FormattingSettingsService();
-        this.target = options.element;
-        this.updateCount = 0;
-        if (document) {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
-        }
+
+    //Constructor
+    constructor(options: VisualContructorOptions){
+        this.svg = d3.select(options.element).append("svg").classed('circleCard', true);
+        this.container = d3.select(options.element).append("g").classed('container',true);
+        this.circle = d3.select(options.element).append("circle").classed('circle', true);
+        this.textValue =  d3.select(options.element).append("text").classed('textValue', true);
+        this.textLabel =  d3.select(options.element).append("text").classed('textLabel', true);
     }
 
     public update(options: VisualUpdateOptions) {
-        this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews[0]);
-
-        console.log('Visual update', options);
-        if (this.textNode) {
-            this.textNode.textContent = (this.updateCount++).toString();
-        }
+        let width: number = options.viewport.width;
+        let height: number = options.viewport.height;
+        this.svg.attr("width", width);
+        this.svg.attr("height", height);
+        let radius: number = Math.min(width, height) / 2.2;
+        this.circle
+            .style("fill", "white")
+            .style("fill-opacity", 0.5)
+            .style("stroke", "black")
+            .style("stroke-width", 2)
+            .attr("r", radius)
+            .attr("cx", width / 2)
+            .attr("cy", height / 2);
+        let fontSizeValue: number = Math.min(width, height) / 5;
+        this.textValue
+            .text("Value")
+            .attr("x", "50%")
+            .attr("y", "50%")
+            .attr("dy", "0.35em")
+            .attr("text-anchor", "middle")
+            .style("font-size", fontSizeValue + "px");
+        let fontSizeLabel: number = fontSizeValue / 4;
+        this.textLabel
+            .text("Label")
+            .attr("x", "50%")
+            .attr("y", height / 2)
+            .attr("dy", fontSizeValue / 1.2)
+            .attr("text-anchor", "middle")
+            .style("font-size", fontSizeLabel + "px");
     }
 
-    /**
-     * Returns properties pane formatting model content hierarchies, properties and latest formatting values, Then populate properties pane.
-     * This method is called once every time we open properties pane or when the user edit any format property. 
-     */
-    public getFormattingModel(): powerbi.visuals.FormattingModel {
-        return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
-    }
 }
+
+
+
+// import powerbi from "powerbi-visuals-api";
+// import { FormattingSettingsService } from "powerbi-visuals-utils-formattingmodel";
+// import "./../style/visual.less";
+
+// import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+// import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+// import IVisual = powerbi.extensibility.visual.IVisual;
+
+// import { VisualFormattingSettingsModel } from "./settings";
+
+// export class Visual implements IVisual {
+//     private target: HTMLElement;
+//     private updateCount: number;
+//     private textNode: Text;
+//     private formattingSettings: VisualFormattingSettingsModel;
+//     private formattingSettingsService: FormattingSettingsService;
+
+//     constructor(options: VisualConstructorOptions) {
+//         console.log('Visual constructor', options);
+//         this.formattingSettingsService = new FormattingSettingsService();
+//         this.target = options.element;
+//         this.updateCount = 0;
+//         if (document) {
+//             const new_p: HTMLElement = document.createElement("p");
+//             new_p.appendChild(document.createTextNode("Update count:"));
+//             const new_em: HTMLElement = document.createElement("em");
+//             this.textNode = document.createTextNode(this.updateCount.toString());
+//             new_em.appendChild(this.textNode);
+//             new_p.appendChild(new_em);
+//             this.target.appendChild(new_p);
+//         }
+//     }
+
+//     public update(options: VisualUpdateOptions) {
+//         this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews[0]);
+
+//         console.log('Visual update', options);
+//         if (this.textNode) {
+//             this.textNode.textContent = (this.updateCount++).toString();
+//         }
+//     }
+
+//     /**
+//      * Returns properties pane formatting model content hierarchies, properties and latest formatting values, Then populate properties pane.
+//      * This method is called once every time we open properties pane or when the user edit any format property. 
+//      */
+//     public getFormattingModel(): powerbi.visuals.FormattingModel {
+//         return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
+//     }
+// }
