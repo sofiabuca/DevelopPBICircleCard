a1a09d1ac270994f94cc903a54526d77fbdafdba
Sofia Buitrago <laurabuca98@gmail.com>
2024-07-27 20:54:55 -0600

    - Instal libraries

---
+++ circleCard/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var circleCard3BE7555484204B7C8C140FB25F6D0DE7_DEBUG: IVisualPlugin;
+export default circleCard3BE7555484204B7C8C140FB25F6D0DE7_DEBUG;
---
+++ circleCard/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,32 @@
+import { Visual } from "../../src/visual";
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions;
+import DialogConstructorOptions = powerbiVisualsApi.extensibility.visual.DialogConstructorOptions;
+var powerbiKey: any = "powerbi";
+var powerbi: any = window[powerbiKey];
+var circleCard3BE7555484204B7C8C140FB25F6D0DE7_DEBUG: IVisualPlugin = {
+    name: 'circleCard3BE7555484204B7C8C140FB25F6D0DE7_DEBUG',
+    displayName: 'CircleCard',
+    class: 'Visual',
+    apiVersion: '5.3.0',
+    create: (options?: VisualConstructorOptions) => {
+        if (Visual) {
+            return new Visual(options);
+        }
+        throw 'Visual instance not found';
+    },
+    createModalDialog: (dialogId: string, options: DialogConstructorOptions, initialState: object) => {
+        const dialogRegistry = (<any>globalThis).dialogRegistry;
+        if (dialogId in dialogRegistry) {
+            new dialogRegistry[dialogId](options, initialState);
+        }
+    },
+    custom: true
+};
+if (typeof powerbi !== "undefined") {
+    powerbi.visuals = powerbi.visuals || {};
+    powerbi.visuals.plugins = powerbi.visuals.plugins || {};
+    powerbi.visuals.plugins["circleCard3BE7555484204B7C8C140FB25F6D0DE7_DEBUG"] = circleCard3BE7555484204B7C8C140FB25F6D0DE7_DEBUG;
+}
+export default circleCard3BE7555484204B7C8C140FB25F6D0DE7_DEBUG;
